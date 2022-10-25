9c572ebfc75a8f6b253c38a7d7e289bd7243c7f2
Rhnkey24 <lawalronke24@gmail.com>
2022-10-25 02:23:51 -0700

    qudus

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,13 @@
 
 int_strlen(char *s)
 {
-	input p = 0;
-	/* incremeant up to when the last character is NULL,\0*/
-	while (*(s + p) != 0)
+	int len = 0;
+
+	while (*s != '\0')
 	{
-		p++;
+		len++;
+		s++;
 	}
-	return (p);
+
+	return (len);
 }
