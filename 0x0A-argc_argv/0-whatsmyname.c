bc9e4ed36ff7b56f1e2a5caa120911c32372454d
Rhnkey24 <lawalronke24@gmail.com>
2022-11-07 00:55:08 -0800

    turn

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,18 @@
-#include "main.h"
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 
 /**
- * main - prints its name
- * @argc: argument count
- * @argv: argument vector
+ * main - fuction
+ * @argc: the int
+ * @argv: the chaine of chaine
  *
  * Return: Always zero
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	(void) argc;
-	printf("argc = %d\n", argc);
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
