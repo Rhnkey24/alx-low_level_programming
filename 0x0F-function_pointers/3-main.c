ab85abf41f90a4fe026aab46dea5409c39230619
Rhnkey24 <lawalronke24@gmail.com>
2022-11-17 10:51:52 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,21 +14,21 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-	printf("Error\n");
-	exit(98);
+		printf("Error\n");
+		exit(98);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	g =  (*get_op_func(argv[2]));
 
-	if (g == NULL || argv[2][1] != '\0')
-	{
-	printf("Error\n");
-	exit(99);
-	}
+		if (g == NULL || argv[2][1] != '\0')
+		{
+		printf("Error\n");
+		exit(99);
+		}
 
-	resul = g(a, b);
+		resul = g(a, b);
 
-	printf("%d\n", resul);
-	return (0);
+		printf("%d\n", resul);
+		return (0);
 }
