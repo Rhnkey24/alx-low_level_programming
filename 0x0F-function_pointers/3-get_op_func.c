05f840cc0ca335905d419a9ebbc5d9f0b0d1587d
Rhnkey24 <lawalronke24@gmail.com>
2022-11-17 10:39:32 -0800

    3-get_op

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,9 +19,9 @@ int (*get_op_func(char *s))(int a, int b)
 	int i = 0;
 
 	while (i < 5)
-	{
-	if (*ops[i].op == *s)
-	return (ops[i].f);
+	
+		if (*ops[i].op == *s)
+			return (ops[i].f);
 	i++;
 	}
 	return (NULL);
