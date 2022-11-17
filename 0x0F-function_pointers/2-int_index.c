775c454550241be994f2b4d1e81ea39c8fa16c95
Rhnkey24 <lawalronke24@gmail.com>
2022-11-17 09:04:00 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,9 +3,24 @@
 
 /**
  *
- *  * * int_index - a function ...
- *   * * @array: the tab of integer
- 
- *    * * @size: the number
- *
- *     * * @cmp: the function * *--6+9+
+ * * int_index - a function ...
+ * * @array: the tab of integer
+ * * @size: the number
+ * * @cmp: the function
+ * * Return: 1 or 0
+ */
+
+int int_index(int *array, size_t size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
